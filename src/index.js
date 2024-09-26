3fedc29e79acfd08551cbeb4651a8965ce8ca040
Coco Greig <cocogreig@hotmail.co.uk>
2024-09-26 17:39:00 +0200

    Lab Solved

--- src/index.js
+++ src/index.js
@@ -7,16 +7,42 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
-
-
+let toastmessage = document.querySelector("#toast-message")
+let startButton = document.querySelector("#start-btn")
+//console.log(startButton)
+startButton.addEventListener("click", () => {startCountdown()})
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
+//  console.log("startCountdown called!");
+showToast("Final Countdown")
 
   // Your code goes here ...
+let timeShown = document.querySelector("#time")
+  let i = remainingTime;
+  const intervalId = setInterval(function () {
+    i--;
+    timeShown.innerText=i;
+    if (i === 5) {
+      //console.log(i);
+      //timeShown.innerText=i;
+      startButton.disabled = true;
+      showToast("Engines Starting")
+    } 
+    
+    
+     if(i === 0) { 
+      timeShown.innerText=i;
+      startButton.disabled = false;
+      clearInterval(intervalId);
+      showToast("Lift Off")
+      
+    }
+  
+    
+  }, 1000);
+  timeShown.innerText=DURATION
 }
 
 
@@ -24,9 +50,16 @@ function startCountdown() {
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
-
-  // Your code goes here ...
+  //console.log("showToast called!");
+  toastmessage.innerText=message
+let toast = document.querySelector(".toast")
+const toastDisplay = toast.classList;
+//toastDisplay.toggle("show");
+toastDisplay.add("show");
+function hideToast() {
+  toastDisplay.remove("show");
+}
+const timeoutId = setTimeout(hideToast, 3000);
 
 
 
@@ -34,5 +67,8 @@ function showToast(message) {
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
+
+  let closeToast = document.querySelector("#close-toast")
+  closeToast.addEventListener("click", () => {hideToast()})
 
 }
